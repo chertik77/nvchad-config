c240ad98f77e1646fe1c2eae87f0a876cff7290d
chertik77 <denysbabych.dev@gmail.com>
2026-03-15 00:49:29 +0000

    fix: minor updates & improvements

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -4,16 +4,3 @@ require 'nvchad.autocmds'
 vim.api.nvim_create_autocmd('BufWritePre', {
 	command = [[%s/\s\+$//e]],
 })
-
--- Organize imports on save
-vim.api.nvim_create_autocmd('BufWritePre', {
-	callback = function()
-		vim.lsp.buf.code_action {
-			context = {
-				only = { 'source.organizeImports' },
-				diagnostics = vim.diagnostic.get(0),
-			},
-			apply = true,
-		}
-	end,
-})
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -101,4 +101,19 @@ return {
 			require 'configs.neocodeium'
 		end,
 	},
+	{
+		'kdheepak/lazygit.nvim',
+		lazy = true,
+		cmd = {
+			'LazyGit',
+			'LazyGitConfig',
+			'LazyGitCurrentFile',
+			'LazyGitFilter',
+			'LazyGitFilterCurrentFile',
+		},
+		dependencies = { 'nvim-lua/plenary.nvim' },
+		keys = {
+			{ '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+		},
+	},
 }
